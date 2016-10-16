e7bca40f3df1785775208135c48fd15901847cdf
MFMS1016 <MFMS1016@gmail.com>
2016-10-16 12:19:21 +0200

    first comming

---
+++ ReportHelper/app/src/main/java/mfms1016/reporthelper/MainActivity.java
@@ -0,0 +1,13 @@
+package mfms1016.reporthelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
